340342af56f8640c717b719a5a0dc3112137b279
Carlos.Luo <luoheng158@gmail.com>
2020-06-14 13:30:56 +0800

    user

--- SpringBootDemo/DevDemo/src/main/java/com/example/DevDemo/controller/LoginController.java
+++ SpringBootDemo/DevDemo/src/main/java/com/example/DevDemo/controller/LoginController.java
@@ -3,14 +3,26 @@ package com.example.DevDemo.controller;
 import com.example.DevDemo.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class LoginController {
 
+    private final AtomicLong userIdGenerator = new AtomicLong();
+    private final List<User> userInfo;
+    private final Object lock = new Object();
+
+    public LoginController() {
+        System.out.println(threadInfo() + ", LoginController Constructor, hashCode: " + hashCode());
+        userInfo = new ArrayList<>();
+    }
+
     @RequestMapping("/login")
-    User login() {
+    public User login() {
         System.out.println("login----->");
         User user = new User();
         user.setTimestamp(System.currentTimeMillis());
@@ -18,4 +30,69 @@ public class LoginController {
         user.setPassword("123456");
         return user;
     }
+
+    @RequestMapping(value = "/user/add", method = RequestMethod.POST)
+    public User add(@RequestBody User user) {
+        System.out.println("tid:" + Thread.currentThread().getId() + ", /user/add: " + user);
+        synchronized (lock) {
+            User findUser = findByUserIdImpl(user.getUserId());
+            if (findUser != null) {
+                return findUser;
+            }
+            user.setTimestamp(System.currentTimeMillis());
+            user.setUserId(userIdGenerator.incrementAndGet());
+            userInfo.add(user);
+            return user;
+        }
+    }
+
+    @RequestMapping(value = "/user/delete", method = RequestMethod.POST)
+    public User delete(@RequestBody User user) {
+        System.out.println(threadInfo() + ", /user/delete: " + user);
+        synchronized (lock) {
+            final User findUser;
+            if ((findUser = findByUserIdImpl(user.getUserId())) == null) {
+                return null;
+            }
+            userInfo.remove(findUser);
+            return findUser;
+        }
+    }
+
+    @RequestMapping(value = "/user/update", method = RequestMethod.POST)
+    public User update(@RequestBody User user) {
+        System.out.println(threadInfo() + ", /user/update: " + user);
+        synchronized (lock) {
+            final User findUser;
+            if ((findUser = findByUserIdImpl(user.getUserId())) == null) {
+                return null;
+            }
+            findUser.setUsername(user.getUsername());
+            findUser.setPassword(user.getPassword());
+            return findUser;
+        }
+    }
+
+    @RequestMapping(value = "/user/query", method = RequestMethod.GET)
+    public User findByUserId(@RequestParam long userId) {
+        System.out.println(threadInfo() + ", /user/query: " + userId);
+        return findByUserIdImpl(userId);
+    }
+
+    private User findByUserIdImpl(long userId) {
+        synchronized (lock) {
+            for (User user : userInfo) {
+                if (user.getUserId() == userId) {
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+
+    private String threadInfo() {
+        final Thread thread = Thread.currentThread();
+        return String.format("[tid: %s, name: %s]", thread.getId(), thread.getName());
+    }
+
 }
--- SpringBootDemo/DevDemo/src/main/java/com/example/DevDemo/model/User.java
+++ SpringBootDemo/DevDemo/src/main/java/com/example/DevDemo/model/User.java
@@ -2,9 +2,18 @@ package com.example.DevDemo.model;
 
 public class User {
 
+    private long userId;
     private long timestamp;
     private String username;
     private String password;
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
 
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
@@ -28,5 +37,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("User{");
+        if (userId > 0) {
+            sb.append("userId=" + userId);
+            sb.append(", timestamp=" + timestamp);
+        } else {
+            sb.append("timestamp=" + timestamp);
+        }
+        sb.append(", username=" + username);
+        sb.append(", password=" + password);
+        sb.append("}");
+        return sb.toString();
     }
 }
